1e93febc00aad5f9d3581487e03246462782a27f
Seb Jacobs <me@sebjacobs.com>
2012-06-19 14:25:14 +0100

    renamed generator to install - so users can run rails g ffcrm_ldap:install

--- lib/generators/ffcrm_ldap/ffcrm_ldap_generator.rb
+++
@@ -1,7 +0,0 @@
-class FfcrmLdapGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-  def copy_config
-    copy_file "config/ldap.yml.example"
-    copy_file "config/ldap_attributes_map.yml.example"
-  end
-end
---
+++ lib/generators/ffcrm_ldap/install_generator.rb
@@ -0,0 +1,9 @@
+module FfcrmLdap
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+    def copy_config
+      copy_file "config/ldap.yml.example"
+      copy_file "config/ldap_attributes_map.yml.example"
+    end
+  end
+end
